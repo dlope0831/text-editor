a3aca6653e1bf45981617acd0b358a65c5e85cc5
Daniela Lopez <dlope0831@gmail.com>
2022-11-21 21:54:46 -0600

    fixed srv sw-js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,46 +1,4 @@
-// const installBtn = document.getElementById('buttonInstall');
-
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-// window.addEventListener('beforeinstallprompt', (event) => {
-//     if (window.matchMedia('(display-mode: standalone)').matches) {
-//       installBtn.style.display = "none";
-//     }
-    
-//     window.addEventListener('beforeinstallprompt', (event) => {
-//       event.preventDefault();
-//       installBtn.style.visibility = 'visible';
-//       installBtn.addEventListener('click', () => {
-//         event.prompt();
-//         installBtn.setAttribute('disabled', true);
-//         installBtn.textContent = 'Installed!';
-//         });
-//       });
-// });
-
-// TODO: Implement a click event handler on the `butInstall` element
-// butInstall.addEventListener('click', async () => {
-//     const promptEvent = window.deferredPrompt;
-  
-//     if (!promptEvent) {
-//       return;
-//     }
-  
-//     promptEvent.prompt();
-  
-//     window.deferredPrompt = null;
-  
-//     butInstall.classList.toggle('hidden', true);
-//   });
-  
-  
-
-// TODO: Add an handler for the `appinstalled` event
-//   window.addEventListener('appinstalled', (event) => {
-//     console.log('👍', 'appinstalled', event);
-//   });
-
-  const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById('buttonInstall');
 
 window.addEventListener('beforeinstallprompt', (event) => {
   window.deferredPrompt = event;
